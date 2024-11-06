9789b6e74aa8aa5a7b6047687dcdefdbe97af03d
HerryWEu <yihong.wu@mhp.com>
2024-11-06 08:23:10 +0800

    feat: redis, 初始化boot3代码

---
+++ boot3-09-redis-stu/src/main/java/com/chloe/redis/Boot309RedisStuApplication.java
@@ -0,0 +1,13 @@
+package com.chloe.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot309RedisStuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot309RedisStuApplication.class, args);
+    }
+
+}
---
+++ boot3-09-redis-stu/src/test/java/com/chloe/redis/Boot309RedisStuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chloe.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot309RedisStuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
