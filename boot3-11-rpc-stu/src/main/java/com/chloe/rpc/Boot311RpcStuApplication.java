f2fbf0c227349587448a14a50e23263064880aab
HerryWEu <yihong.wu@mhp.com>
2024-11-06 16:17:10 +0800

    feat: rpc 初始化

---
+++ boot3-11-rpc-stu/src/main/java/com/chloe/rpc/Boot311RpcStuApplication.java
@@ -0,0 +1,13 @@
+package com.chloe.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot311RpcStuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot311RpcStuApplication.class, args);
+    }
+
+}
---
+++ boot3-11-rpc-stu/src/test/java/com/chloe/rpc/Boot311RpcStuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chloe.rpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot311RpcStuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
