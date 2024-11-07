ea4ea6916846b410d271de9d9653ef5418465ff8
HerryWEu <yihong.wu@mhp.com>
2024-11-07 22:41:30 +0800

    feat: AOT 打包到Windows平台和Liunx平台

---
+++ boot3-15-aot-common-stu/src/main/java/com/chloe/MainApplication.java
@@ -0,0 +1,16 @@
+package com.chloe;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.chloe
+ * Description:
+ *
+ * @Author Xu, Luqin
+ * @Create 2024/11/7 19:34
+ * @Version 1.0
+ */
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
