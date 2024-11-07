6659a029a415fdcb12959b8f5e2566b25dff97f6
HerryWEu <yihong.wu@mhp.com>
2024-11-07 08:18:47 +0800

    feat: Web安全 -初始化

---
+++ boot3-13-security-stu/src/main/java/com/chloe/security/Boot313SecurityStuApplication.java
@@ -0,0 +1,13 @@
+package com.chloe.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot313SecurityStuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot313SecurityStuApplication.class, args);
+	}
+
+}
---
+++ boot3-13-security-stu/src/test/java/com/chloe/security/Boot313SecurityStuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chloe.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot313SecurityStuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
