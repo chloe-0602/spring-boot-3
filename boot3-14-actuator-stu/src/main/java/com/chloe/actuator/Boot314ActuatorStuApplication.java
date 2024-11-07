cfd94b039ea6653f2485533ef035c10cf274ff68
HerryWEu <yihong.wu@mhp.com>
2024-11-07 10:47:07 +0800

    feat: 可观测性- 初始化

---
+++ boot3-14-actuator-stu/src/main/java/com/chloe/actuator/Boot314ActuatorStuApplication.java
@@ -0,0 +1,13 @@
+package com.chloe.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot314ActuatorStuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot314ActuatorStuApplication.class, args);
+	}
+
+}
---
+++ boot3-14-actuator-stu/src/test/java/com/chloe/actuator/Boot314ActuatorStuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chloe.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot314ActuatorStuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
